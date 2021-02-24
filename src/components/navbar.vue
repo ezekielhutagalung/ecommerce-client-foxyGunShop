d225f5a90cfba9e7286f898f0d0ee474a9b63652
ezekielhutagalung <yehezkiel.life@gmail.com>
2021-02-25 05:52:44 +0700

    Clientbenerbenerfinal

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,16 +1,35 @@
 <template>
 <div>
-  <nav class="navbar navbar-expand-lg navbar-light bg-custom" id="navbaratas">
-    <div class="container-fluid">
-      <a class="navbar-brand" style="color: white; font-size: 25px" id="nav-logo"><img src="./fox-logo-png-1658.png" style="width:100px; height:50px"></a>
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-         <button class="btn btn-outline-light" id="nav1" @click.prevent="goToCart">Cart</button>
-         <button class="btn btn-outline-light" id="nav2" style="margin-left:20px" @click.prevent="goToHome">Home</button>
+ <nav class="navbar navbar-expand-lg navbar-light" id="navbaratas">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+       <button class="btn btn-outline-light" id="nav1" style="margin-left:20px"  @click.prevent="goToCart">Cart</button>
+      </li>
+      <li class="nav-item">
+       <button class="btn btn-outline-light" id="nav2" style="margin-left:20px" @click.prevent="goToHome">Home</button>
+      </li>
+      <li class="nav-item dropdown">
         <button class="btn btn-outline-light" id="nav3" style="margin-left:20px" @click.prevent="goSignOut" v-if="logStatus === true">Sign Out</button>
-      </ul>
-    </div>
-  </nav>
+          <a>
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+    </ul>
+     <a class="navbar-brand" style="color: white; font-size: 25px margin-right:29px" id="nav-logo"><img src="./fox-logo-png-1658.png" style="width:100px; height:50px"></a>
+  </div>
+</nav>
 </div>
+
 </template>
 
 <script>
@@ -45,7 +64,8 @@ export default {
 
 <style>
 #navbaratas{
-border-radius: 25px;
+background-color: transparent;
+  border-radius: 25px;
 box-shadow: 0px 0px 10px #81007c, 0px 0px 30px #81007c;
 }
 
